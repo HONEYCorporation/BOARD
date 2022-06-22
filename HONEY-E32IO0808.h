add62e8110de10649ef9846c5a54990dfe5f8805
Sonthaya Boonchan <sonthaya.b@honey.co.th>
2022-06-22 15:20:08 +0700

    Update HONEY-E32IO0808.h

--- HONEY-E32IO0808.h
+++ HONEY-E32IO0808.h
@@ -6,7 +6,7 @@ uint8_t PIN_BTN = 0;
 uint8_t X[] = {26, 25, 33, 32, 35, 34, 39, 36};
 uint8_t Y[] = {23, 19, 18, 5, 4, 13, 14, 27};
 
-void io_setup()
+void _setup()
 {
     pinMode(PIN_BTN,INPUT_PULLUP);
     pinMode(PIN_RUN, OUTPUT);
